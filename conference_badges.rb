6d1c597d24129cf91ced782dbabf718ec614bed8
Quinn Cox <qrc.inpa@gmail.com>
2019-02-28 00:49:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,11 @@
 # Write your code here.
+attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |name|
