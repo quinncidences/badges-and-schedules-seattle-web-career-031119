2c0a62005c3e86a4cf08e5d8ce51de80d94d3282
Quinn Cox <qrc.inpa@gmail.com>
2019-02-28 01:09:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges.push("Hello, my name is #{name}.")
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  counter = 1
+  attendees.each do |name|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |line|
+    puts line
+  end
+  assign_rooms(attendees).each do |line|
+    puts line
+  end
+end
